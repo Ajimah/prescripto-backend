981a13c587450294fdcc78ed7131453888f80fea
kenechukwu ajimah <kenechukwuajimah@gmail.com>
2025-01-24 14:38:38 +0100

    dashboard & appointments added

--- controller/adminController.js
+++ controller/adminController.js
@@ -3,7 +3,8 @@ import bcrypt from "bcrypt"
 import {v2 as cloudinary } from "cloudinary"
 import doctorModel from "../models/doctorModel.js"
 import jwt from 'jsonwebtoken'
-
+import appointmentModel from "../models/appointmentModel.js"
+import userModel from "../models/userModel.js"
 
 // Api for adding doctors 
 const addDoctor = async (req,res) => {
@@ -108,5 +109,83 @@ const allDoctors = async (req, res) => {
 }
 
 
+//api to get all appointment list
 
-export {addDoctor,LoginAdmin, allDoctors}
+
+const appointmentAdmin = async (req,res) => {
+    try {
+        const appointments = await appointmentModel.find({})
+        res.json({success:true,appointments})
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:error.message})
+    }
+}
+
+//api for appointment cancelation
+
+const appointmentCancel = async (req,res) => {
+    try {
+        
+        const {appointmentId} = req.body
+        const appointmentData = await appointmentModel.findById(appointmentId)
+
+       
+
+        await appointmentModel.findByIdAndUpdate(appointmentId,{cancelled:true})
+
+
+
+        //releasing doctor slots
+
+        const {docId, slotDate, slotTime} = appointmentData
+
+        const doctorData = await doctorModel.findById(docId)
+
+
+        let slots_booked = doctorData.slots_booked
+
+        slots_booked[slotDate] = slots_booked[slotDate].filter(e => e !== slotTime)
+
+        await doctorModel.findByIdAndUpdate(docId, {slots_booked})
+        res.json({success:true, message:'Appointment cancelled successfully'})
+
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({success:false, message:error.message})
+    }
+
+
+}
+
+
+// api to get dashboard data for admin panel 
+
+const adminDashboard = async (req,res) => {
+    try {
+        const doctors = await doctorModel.find({})
+        const users = await userModel.find({})
+        const appointment = await appointmentModel.find({})
+        
+        const dashData = {
+            doctors: doctors.length,
+            appointment: appointment.length,
+            patients: users.length,
+            latestAppointments:  appointment.reverse().slice(0,5)
+            
+        }
+        
+        res.json({success:true , dashData})
+        
+        console.log(dashData)
+        
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({success:false, message:error.message})
+    }
+}
+
+
+
+export {addDoctor,LoginAdmin, allDoctors,appointmentAdmin,appointmentCancel,adminDashboard }
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addDoctor, allDoctors, LoginAdmin } from "../controller/adminController.js";
+import { addDoctor, allDoctors, LoginAdmin,appointmentAdmin, appointmentCancel , adminDashboard } from "../controller/adminController.js";
 import upload from "../middleware/multer.js";
 import authAdmin from "../middleware/authAdmin.js";
 import {changeAvailability} from '../controller/doctorController.js'
@@ -15,14 +15,11 @@ adminRouter.post("/login", LoginAdmin);
 adminRouter.post("/all-doctors",authAdmin ,allDoctors);
 adminRouter.post("/change-availability",authAdmin ,changeAvailability);
 adminRouter.post('/getAllTransactions/',authAdmin, getAllTransactions);
+adminRouter.get('/appointments', authAdmin,appointmentAdmin);
+adminRouter.post('/cancel-appointment',authAdmin,appointmentCancel);
+adminRouter.get('/dashboard', authAdmin,adminDashboard )
 
 
 export default adminRouter
 
-// import express from "express";
-// import { createPost, deletePost, getAllPosts, getSinglePost, likeUnlikePost, replyToPost } from "../../controllers/post.controller.js";
-// import protectRoute from "../../middlewares/protectRoute.js";
-// import upload from "../../config/multer.js";
-// const router = express.Router()
 
-// router.post("/add", protectRoute, upload.single('img'), createPost);
